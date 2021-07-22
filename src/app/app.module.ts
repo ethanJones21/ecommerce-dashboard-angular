eabce6d7bc11400bd86318ae200a5a50630b9c8d
Roberto Luna Victoria Monzon <roberto971968@hotmail.com>
2021-07-22 12:46:46 -0500

    clase 9, solo instalacion de @auth0/jwt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { SharedModule } from './shared/components/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, InicioComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
