3ce0b27fb06f68f39489307dea453ba60cef679d
Roberto Luna Victoria Monzon <roberto971968@hotmail.com>
2021-07-22 12:52:57 -0500

    cambiando nombre del proyecto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ecommerce-model'`, () => {
+  it(`should have as title 'ecommerce-dashboard-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ecommerce-model');
+    expect(app.title).toEqual('ecommerce-dashboard-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ecommerce-model app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'ecommerce-dashboard-angular app is running!'
+    );
   });
 });
