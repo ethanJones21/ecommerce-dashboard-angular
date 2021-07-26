d16aaeb3ce96b55bfb8402255064faead8495442
Roberto Luna Victoria Monzon <roberto971968@hotmail.com>
2021-07-26 14:39:36 -0500

    sidebar y cart component

--- src/app/shared/components/shared.module.ts
+++ src/app/shared/components/shared.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoadingComponent } from './loading/loading.component';
+import { SkeletonTableLoadComponent } from './skeleton-table-load/skeleton-table-load.component';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [SidebarComponent, NavbarComponent],
-  imports: [CommonModule],
-  exports: [SidebarComponent, NavbarComponent],
+  declarations: [
+    SidebarComponent,
+    NavbarComponent,
+    LoadingComponent,
+    SkeletonTableLoadComponent,
+  ],
+  imports: [CommonModule, RouterModule, NgxSkeletonLoaderModule],
+  exports: [
+    SidebarComponent,
+    NavbarComponent,
+    LoadingComponent,
+    SkeletonTableLoadComponent,
+  ],
 })
 export class SharedModule {}
