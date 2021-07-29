93a3a93962a34b26df10787d360eb75fa55b1c41
Roberto Luna Victoria Monzon <roberto971968@hotmail.com>
2021-07-28 23:08:39 -0500

    get clients and paginations complete

--- src/app/pages/clients/clients.component.ts
+++ src/app/pages/clients/clients.component.ts
@@ -1,50 +1,46 @@
 import {
-  AfterViewInit,
   Component,
   ElementRef,
   OnDestroy,
   OnInit,
   QueryList,
-  Renderer2,
   ViewChildren,
 } from '@angular/core';
-import { ClientsService } from './services/clients.service';
 import { Observable, Subscription } from 'rxjs';
-import { delay, map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+
+import { ClientsService } from './services/clients.service';
 
 @Component({
   selector: 'Clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.scss'],
 })
-export class ClientsComponent implements OnInit, OnDestroy, AfterViewInit {
+export class ClientsComponent implements OnInit, OnDestroy {
   term = '';
-  pages: number[] = []; //longitud
+  pagination = {
+    pages: [1],
+    longitud: 1,
+    previous: 0,
+    next: 2,
+    limit: 1,
+  };
 
   clients$!: Observable<any[]>;
   subs = new Subscription();
   pagesEl: ElementRef[] = [];
+
   limit = 10;
-  previous = 1;
-  next = 1;
-  isLoading = true;
   notPrevPage = false;
   notNextPage = false;
 
   @ViewChildren('paginaCliente', { read: ElementRef })
   paginaCliente!: QueryList<ElementRef>;
 
-  constructor(
-    private clientsServ: ClientsService,
-    private renderer: Renderer2
-  ) {}
+  constructor(private clientsServ: ClientsService) {}
 
   ngOnInit(): void {
     this.getClients('', 1);
-  }
-
-  ngAfterViewInit(): void {
-    this.getPagesEl();
   }
 
   ngOnDestroy(): void {
@@ -55,61 +51,13 @@ export class ClientsComponent implements OnInit, OnDestroy, AfterViewInit {
     return this.getClients(termino, 1);
   }
 
-  private selectedPage(page: number): void {
-    //empieza desde cero [0]
-    if (this.pagesEl.length > 0) {
-      this.renderer.addClass(this.pagesEl[page - 1].nativeElement, 'selected');
-    }
-  }
-
-  private removeAllSelectedPages(): void {
-    if (this.pagesEl.length > 0) {
-      this.pagesEl.forEach((val, i) => {
-        this.renderer.removeClass(this.pagesEl[i].nativeElement, 'selected');
-      });
-    }
-  }
-
-  private getPagesEl(): void {
-    //una sola vez
-    this.subs.add(
-      this.paginaCliente.changes.subscribe((paginas) => {
-        paginas.forEach(
-          (pagina: ElementRef, i: number) => (this.pagesEl[i] = pagina)
-        );
-        this.selectedPage(1);
-      })
-    );
-  }
-
-  private conditionalsControls(previous: any, next: any, longitud: number) {
-    if (typeof previous === 'number') {
-      this.previous = previous;
-      if (previous == 0) this.notPrevPage = true;
-    } else {
-      this.previous = previous.page;
-      this.notPrevPage = false;
-    }
-
-    if (typeof next === 'number') {
-      this.next = next;
-      if (this.next == longitud) this.notNextPage = true;
-    } else {
-      this.next = next.page;
-      this.notNextPage = false;
-    }
-  }
-
   getClients(term: any, page: number) {
     this.term = term;
     this.clients$ = this.clientsServ.getClients(term, page, this.limit).pipe(
       // delay(3000),
-      map(({ clients, pages, longitud, previous, next }) => {
-        this.pages = pages;
-        this.conditionalsControls(previous, next, longitud);
-        this.removeAllSelectedPages();
-        this.selectedPage(page);
-        this.isLoading = false;
+      map(({ clients, ...data }) => {
+        this.pagination = { ...data };
+        this.pagination.limit = this.limit;
         return clients;
       })
     );
